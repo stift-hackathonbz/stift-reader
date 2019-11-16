c6ed46e55ea4db64cc6fc972e9906cc2f591dbc1
Christian Pitscheider <christian.pitscheider@polito.it>
2019-11-16 10:10:41 +0100

    added RFID to wifi

--- StiftReaderWiFi/StiftReaderWiFi.ino
+++ StiftReaderWiFi/StiftReaderWiFi.ino
@@ -9,10 +9,48 @@
  modified 31 May 2012
  by Tom Igoe
  */
+#include <Wire.h>
 #include <SPI.h>
+#include <Adafruit_PN532.h>
 #include <WiFiNINA.h>
 
 #include "arduino_secrets.h" 
+
+// If using the breakout with SPI, define the pins for SPI communication.
+#define PN532_SCK  (2)
+#define PN532_MOSI (3)
+#define PN532_SS   (4)
+#define PN532_MISO (5)
+
+// If using the breakout or shield with I2C, define just the pins connected
+// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
+#define PN532_IRQ   (2)
+#define PN532_RESET (3)  // Not connected by default on the NFC Shield
+
+// Uncomment just _one_ line below depending on how your breakout or shield
+// is connected to the Arduino:
+
+// Use this line for a breakout with a SPI connection:
+//Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+
+// Use this line for a breakout with a hardware SPI connection.  Note that
+// the PN532 SCK, MOSI, and MISO pins need to be connected to the Arduino's
+// hardware SPI SCK, MOSI, and MISO pins.  On an Arduino Uno these are
+// SCK = 13, MOSI = 11, MISO = 12.  The SS line can be any digital IO pin.
+//Adafruit_PN532 nfc(PN532_SS);
+
+// Or use this line for a breakout or shield with an I2C connection:
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+
+#if defined(ARDUINO_ARCH_SAMD)
+// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
+// also change #define in Adafruit_PN532.cpp library file
+   #define Serial SerialUSB
+#endif
+
+
+
+
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = "openAiR";        // your network SSID (name)
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
@@ -28,6 +66,11 @@ char server[] = "stift-server.herokuapp.com";
 
 void setup() {
   //Initialize serial and wait for port to open:
+  #ifndef ESP8266
+    while (!Serial); // for Leonardo/Micro/Zero
+  #endif
+
+  
   Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -58,24 +101,93 @@ void setup() {
   Serial.print("You're connected to the network");
   printCurrentNet();
   printWifiData();
+
+
+
+  
+  Serial.begin(115200);
+  Serial.println("Hello!");
+
+  nfc.begin();
+
+  
+  Serial.println("NFC OK!");
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  // Set the max number of retry attempts to read from a card
+  // This prevents us from waiting forever for a card, which is
+  // the default behaviour of the PN532.
+  nfc.setPassiveActivationRetries(0xFF);
+  
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+  
+  Serial.println("Waiting for an ISO14443A card");
 }
 
 
-void loop() {
-  // if there's incoming data from the net connection.
-  // send it out the serial port.  This is for debugging
-  // purposes only:
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
+//void loop() {
+//  // if there's incoming data from the net connection.
+//  // send it out the serial port.  This is for debugging
+//  // purposes only:
+//  while (client.available()) {
+//    char c = client.read();
+//    Serial.write(c);
+//  }
+//
+//  // if ten seconds have passed since your last connection,
+//  // then connect again and send data:
+//  if (millis() - lastConnectionTime > postingInterval) {
+//    httpRequest();
+//  }
+//
+//}
 
-  // if ten seconds have passed since your last connection,
-  // then connect again and send data:
-  if (millis() - lastConnectionTime > postingInterval) {
-    httpRequest();
-  }
 
+
+void loop(void) {
+  boolean success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+  
+  if (success) {
+    Serial.println("Found a card!");
+    Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("UID Value: ");
+    String stringOne = "";
+    for (uint8_t i=0; i < uidLength; i++) 
+    {
+      Serial.print(" 0x");Serial.print(uid[i], HEX); 
+      String hex = String(uid[i], HEX);
+      stringOne.concat(hex);
+    }
+    Serial.println("");
+    Serial.println(stringOne);
+    
+    httpRequest(stringOne);
+  // Wait 1 second before continuing
+  delay(1000);
+  }
+  else
+  {
+    // PN532 probably timed out waiting for a card
+    Serial.println("Timed out waiting for a card");
+  }
 }
 
 void printWifiData() {
@@ -139,21 +251,25 @@ void printMacAddress(byte mac[]) {
 
 
 // this method makes a HTTP connection to the server:
-void httpRequest() {
+void httpRequest(String UID) {
   // close any connection before send a new request.
   // This will free the socket on the Nina module
   client.stop();
+
+  String post = "POST /api/rfid/";
+  post.concat(UID);
+  post.concat(" HTTP/1.1");
 
   // if there's a successful connection:
   if (client.connect(server, 80)) {
     Serial.println("connecting...");
     // send the HTTP PUT request:
-    client.println("POST /api/rfid/111111 HTTP/1.1");
+    client.println(post);
     client.println("Host: stift-server.herokuapp.com");
     client.println("User-Agent: ArduinoWiFi/1.1");
     client.println("Connection: close");
     client.println();
-
+    Serial.println(post);
     //int statusCode = client.responseStatusCode();
     //client.port();
     //  Serial.print("Status code: ");
@@ -164,6 +280,11 @@ void httpRequest() {
 
     // note the time that the connection was made:
     lastConnectionTime = millis();
+
+    while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
   } else {
     // if you couldn't make a connection:
     Serial.println("connection failed");
